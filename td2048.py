149b1726c5ed60e8cb6a42d9f8f23eaad48df5d9
Jonas Sicking <jonas@sicking.cc>
2018-01-24 19:45:22 -0800

    Minor perf optimizations

--- td2048.py
+++ td2048.py
@@ -5,83 +5,84 @@ import numpy as np
 import board as Board
 import pdb
 
-def board_to_array(board):
-  res = []
-  for _ in range(16):
-    res.append([(board & 0xf) / 13])
+def board_to_array(board, arr, col):
+  for i in range(16):
+    arr[i, col] = (board & 0xf) / 13
     board >>= 4
-  return res
 
 def setup_network():
   n_x = 16
-  m = 1
-  n = [n_x, 30, 30, 20, 10, 1]
-  alpha = 0.005
+  n_y = 1
+  n = [n_x, 30, 30, 10, n_y]
+  alpha = 0.1
 
-  X = tf.placeholder(tf.float32, shape=(n_x, m), name='X')
-  Y = tf.placeholder(tf.float32, shape=(n[-1], m), name='Y')
-  vars = {}
+  X = tf.placeholder(tf.float32, shape=(n_x, None), name='X')
+  Y = tf.placeholder(tf.float32, shape=(n_y, None), name='Y')
   prev_activ = X
   for l in range(1, len(n)):
-    vars["W" + str(l)] = tf.get_variable("W" + str(l), [n[l], n[l-1]], initializer = tf.contrib.layers.xavier_initializer())
-    vars["b" + str(l)] = tf.get_variable("b" + str(l), [n[l], 1], initializer = tf.zeros_initializer())
-    Zl = tf.matmul(vars["W" + str(l)], prev_activ) + vars["b" + str(l)]
+    Wl = tf.get_variable("W" + str(l), [n[l], n[l-1]], dtype=tf.float32, initializer = tf.contrib.layers.xavier_initializer())
+    bl = tf.get_variable("b" + str(l), [n[l], 1], dtype=tf.float32, initializer = tf.zeros_initializer())
+    Zl = tf.matmul(Wl, prev_activ) + bl
     prev_activ = tf.nn.relu(Zl)
 
   cost = tf.nn.l2_loss(prev_activ-Y)
   optimizer = tf.train.AdamOptimizer(learning_rate=alpha).minimize(cost)
 
-  return vars, X, Y, prev_activ, optimizer
+  return X, Y, prev_activ, optimizer
 
 
-variables, X_param, Y_param, yhat, optimizer = setup_network()
+X_param, Y_param, yhat, optimizer = setup_network()
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 Board.print_spacing()
 
+eval_arrays = [np.zeros((16, 1)),
+               np.zeros((16, 2)),
+               np.zeros((16, 3)),
+               np.zeros((16, 4))]
+prev_arr = np.zeros((16, 1))
 
 while True:
   board = Board.comp_move(0)
-  prev_arr = board_to_array(board)
+  board_to_array(board, prev_arr, 0)
 
   while True:
     board = Board.comp_move(board)
-    #Board.print(board, overwrite=False)
 
-    bestval = float("-inf")
-    bestarr = None
+    bestcol = None
     bestboard = None
-    bestdir = None
+
+    boards = []
 
     for dir in range(4):
       newboard = Board.slide(board, dir)
-      if newboard == board:
-        continue
+      if newboard != board:
+        boards.append(newboard)
 
-      arr = board_to_array(newboard)
-      val = sess.run(yhat, feed_dict = { X_param: arr })[0][0]
-      if val > bestval:
-        bestval = val
-        bestarr = arr
-        bestboard = newboard
-        bestdir = dir
-
-    #print(f"bestdir: {bestdir}, bestval: {bestval}")
+    if (len(boards)):
+      arr = eval_arrays[len(boards) - 1]
+      for i, newboard in enumerate(boards):
+        board_to_array(newboard, arr, i)
+      vals = sess.run(yhat, feed_dict = { X_param: arr })[0]
+      bestval = float("-inf")
+      for i, val in enumerate(vals):
+        if val > bestval:
+          bestval = val
+          bestcol = i
+          bestboard = newboard
 
     # learn
-    prev_exp_val = bestval + 1 if bestarr else 0
+    prev_exp_val = bestval + 1 if bestboard else 0
     sess.run(optimizer, feed_dict = { X_param: prev_arr, Y_param: [[prev_exp_val]] })
 
     if bestboard == None:
-      #Board.print(board, overwrite=False)
-      #pdb.set_trace()
       break
 
-    prev_arr = bestarr
+    np.copyto(prev_arr[:, 0], eval_arrays[len(boards) - 1][:, bestcol])
     board = bestboard
 
   Board.print(board)
 
 
-sess.close()
+#sess.close()
